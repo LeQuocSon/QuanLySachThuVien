daa629b3bb422dafb9e3d39d668265cf5a076be7
SonLe <lequocson1997@gmail.com>
2018-06-05 21:19:41 +0700

    Them sinh vien va chinh sua sinh vien

---
+++ AppThuVien/SuaSV.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace AppThuVien
+{
+    partial class SuaSV
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cbbKhoa = new System.Windows.Forms.ComboBox();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtMSSV = new System.Windows.Forms.TextBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // cbbKhoa
+            // 
+            this.cbbKhoa.FormattingEnabled = true;
+            this.cbbKhoa.Location = new System.Drawing.Point(80, 149);
+            this.cbbKhoa.Name = "cbbKhoa";
+            this.cbbKhoa.Size = new System.Drawing.Size(205, 21);
+            this.cbbKhoa.TabIndex = 36;
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(163, 193);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 35;
+            this.btnExit.Text = "Thoát";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(69, 193);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 34;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(26, 152);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(32, 13);
+            this.label4.TabIndex = 33;
+            this.label4.Text = "Khoa";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(26, 111);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 13);
+            this.label3.TabIndex = 32;
+            this.label3.Text = "MSSV";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(26, 70);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 13);
+            this.label2.TabIndex = 31;
+            this.label2.Text = "Họ tên";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(44, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(194, 25);
+            this.label1.TabIndex = 30;
+            this.label1.Text = "Thông tin sinh viên";
+            // 
+            // txtMSSV
+            // 
+            this.txtMSSV.Location = new System.Drawing.Point(80, 108);
+            this.txtMSSV.Name = "txtMSSV";
+            this.txtMSSV.Size = new System.Drawing.Size(205, 20);
+            this.txtMSSV.TabIndex = 29;
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(80, 67);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(205, 20);
+            this.txtName.TabIndex = 28;
+            // 
+            // SuaSV
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(340, 264);
+            this.Controls.Add(this.cbbKhoa);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtMSSV);
+            this.Controls.Add(this.txtName);
+            this.Name = "SuaSV";
+            this.Text = "SuaSV";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cbbKhoa;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtMSSV;
+        private System.Windows.Forms.TextBox txtName;
+    }
+}
---
+++ AppThuVien/SuaSV.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AppThuVien
+{
+    public partial class SuaSV : Form
+    {
+        QuanLyThuVienEntities db = new QuanLyThuVienEntities();
+        private int ID;
+        public SuaSV(int id)
+        {
+            InitializeComponent();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            var result = db.SinhViens.Find(ID);
+            result.Hoten = txtName.Text;
+            result.MSSV = txtMSSV.Text;
+            result.Khoa = cbbKhoa.Text;
+            db.Entry(result).State = EntityState.Modified;
+            db.SaveChanges();
+            this.Close();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        private void SuaSV_Load(object sender, EventArgs e)
+        {
+            var result = db.SinhViens.Find(ID);
+            txtName.Text = result.Hoten;
+            txtMSSV.Text = result.MSSV;
+            cbbKhoa.Text = result.Khoa;
+
+            var lst = db.Khoas.ToList();
+            foreach (var item in lst)
+            {
+                cbbKhoa.Items.Add(new { Text = item.TenKhoa, Value = "1" });
+            }
+
+            cbbKhoa.DisplayMember = "Text";
+            cbbKhoa.ValueMember = "Value";
+        }
+    }
+}
